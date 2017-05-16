12d5012b4ca3ffdbd7394651dab076e954f6452c
jferdousAl <juena.ferdous@alfresco.com>
2017-05-16 14:20:21 +0100

    created cookbook, changes in Gemfile, rakefile and kitchen.yml

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,22 @@
 name 'chef-aps-appserver'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
+maintainer 'Alfresco T&A Team'
+maintainer_email 'devops@alfresco.com'
+license 'Apache 2.0'
 description 'Installs/Configures chef-aps-appserver'
 long_description 'Installs/Configures chef-aps-appserver'
 version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/chef-aps-appserver/issues'
+issues_url 'https://github.com/Alfresco/chef-aps-appserver/issues'
 
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/chef-aps-appserver'
+source_url 'https://github.com/Alfresco/chef-aps-appserver'
+
+chef_version '~> 12'
+supports 'centos', '>= 7.0'
