60a552f8e4cb856356d348ca632b4f48a20bb66b
jferdousAl <juena.ferdous@alfresco.com>
2017-05-22 14:59:40 +0100

    inspec profile

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,5 @@
 # appserver specific attributes
 default['appserver']['engine'] = 'tomcat'
 default['appserver']['aps']['installname'] = 'tomcat'
+default['appserver']['username'] = 'tomcat'
+default['appserver']['group'] = 'tomcat'
