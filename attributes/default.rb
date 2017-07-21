99548005a605a2c6c427d802d6d3f5b21c1b142d
Marco Mancuso <marco.mancuso@alfresco.com>
2017-07-21 15:29:54 +0100

    enabling JMX

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,6 @@ default['appserver']['installname'] = 'tomcat'
 default['appserver']['username'] = 'tomcat'
 default['appserver']['group'] = 'tomcat'
 default['appserver']['home'] = '/usr/share/tomcat'
+
+default['appserver']['jmxremote_databag'] = 'credentials'
+default['appserver']['jmxremote_databag_items'] = %w(systemsmonitor systemscontrol)
